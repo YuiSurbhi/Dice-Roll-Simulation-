2eb58311d5a30a92de2e9e20a7eeb1b8432ec050
Pedini Jayashree <134771316+pedinistar@users.noreply.github.com>
2024-08-07 06:48:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,44 +2,37 @@ import tkinter as tk
 from PIL import Image, ImageTk
 import random
 
-# Create the main window 
 window = tk.Tk()
 window.geometry("500x360")
-window.title("My Two Dice Rollers")
+window.config(pady=30)
+window.title("Dice Roll")
 
-# List of file path for dice images 
-dice = ["C:/Users/HP/Pictures/dice1.png","C:/Users/HP/Pictures/dice2.png","C:/Users/HP/Pictures/dice3.png","C:/Users/HP/Pictures/dice4.png","C:/Users/HP/Pictures/dice5.png","C:/Users/HP/Pictures/dice6.png"]
-
-# Loading the initial images for dice 
+dice = ["dice-six-faces-one.png","dice-six-faces-two.png","dice-six-faces-three.png","dice-six-faces-four.png","dice-six-faces-five.png","dice-six-faces-six.png"]
 image1 = ImageTk.PhotoImage(Image.open(random.choice(dice)))
 image2 = ImageTk.PhotoImage(Image.open(random.choice(dice)))
 
-# Create labels to display dice images 
 label1 = tk.Label(window,image=image1)
 label2 = tk.Label(window,image=image2)
 
-label1.image = image1     # Keep a reference to avoid garbage collection
-label2.image = image2     # Keep a reference to avoid garbage collection
+label1.image = image1
+label2.image = image2
 
-# Place the dice image labels on the window 
-label1.place(x = 40, y = 100)
+label1.place(x = 50, y = 100)
 label2.place(x = 300,y = 100)
 
-# create function to roll the dice and update the labels with new images 
 def dice_roll():
     image1 = ImageTk.PhotoImage(Image.open(random.choice(dice)))
     label1.configure(image=image1)
-    label1.image = image1     # Keep the reference to the new image 
+    label1.image = image1 
 
     image2 = ImageTk.PhotoImage(Image.open(random.choice(dice)))
     label2.configure(image=image2)
-    label2.image = image2     # Keep the reference to the new image 
+    label2.image = image2
 
-# Create and place the roll button attaching roll_dice function 
 button = tk.Button(window,text="ROLL", bg="black",fg = "white",font="Time 20 bold",command=dice_roll)
 button.place(x = 200, y = 0)
 
-# Start the tkinter event loop
+
 window.mainloop()
 
 
